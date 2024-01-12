2505af9d32caa85f3cb8898174e3bf6fd0a3ea1c
Md.Sazid <115614150+mdsazid123@users.noreply.github.com>
2024-01-12 19:08:31 +0530

    create a project cheaking reasult

---
+++ tell.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    int num=11;
+    if (num%2==0)
+    {
+        cout<<"it is even number";
+
+    }
+    else{
+        cout<<"it is odd number";
+
+    }
+    return 0;
+}
